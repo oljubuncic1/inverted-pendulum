2eab75cb7b924c269f7047fd7c0244b830276665
Baki Bakic <baki@baki.ba>
2017-02-22 18:38:08 +0000

    replace baki controller

---
+++ controller/controller.py
@@ -0,0 +1,69 @@
+import time
+from math import pi
+import serial
+from baki_fuzzy_inference import get_controller as fake_ctrl
+import signal
+import sys
+
+def scale_output(x, factor=1.):
+    return x * factor
+
+def normalize_theta(theta):
+    return theta * pi / 180.0
+
+def graceful_shutdown(signal, frame):
+    print "Shutting down gracefully, as BAKI deserved..."
+    for i in range(10):
+        ser.write('0')
+    ser.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, graceful_shutdown)
+signal.signal(signal.SIGTERM, graceful_shutdown)
+
+ser = serial.Serial('/dev/ttyACM0', 115200)
+while ser.isOpen() == False:
+    ser.open()
+
+print 'Connection established, sleeping 10 sec...'
+time.sleep(10)
+print 'Woke up (not like Fake)'
+
+sleep_time = 0.1
+
+ctrl = fake_ctrl()
+
+prev_theta = 0
+theta = 'A0'
+last_time = 0
+
+while 1:
+    try:
+        temp_time = time.time()
+        dtime = temp_time - last_time
+        last_time = temp_time
+        theta = float(theta[1:])
+        theta = normalize_theta(theta)
+        dtheta = (theta - prev_theta) / dtime
+
+        original_output_force = ctrl.output({'theta': theta, 'dtheta': dtheta})['force']
+        original_output_force = original_output_force * 100 / 66.0
+        if original_output_force == -1024:
+            continue
+        prev_theta = theta
+        output_force = abs(original_output_force) * 100
+        if original_output_force < 0:
+            output_force = output_force + 100
+
+        output_force = int(output_force)
+        print '(output_force, theta, prev_theta, dtheta) = (', output_force, theta, prev_theta, dtheta, '); sending force...'
+        ser.write(str(output_force))
+        if ser.inWaiting():
+            theta = ser.readline()
+        else:
+            theta = 'A0'
+            print 'Did not receive anything', theta, dtheta
+        time.sleep(sleep_time)
+    except Exception as e:
+            print 'Exception', e
+            break
